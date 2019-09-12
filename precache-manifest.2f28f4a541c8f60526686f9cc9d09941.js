33369e0029a2f53ca8a6da0ea87644316ff8059a
Lari Mikael Orpana <lari.m.orpana@student.jyu.fi>
2019-09-12 18:48:45 +0300

    Updates

---
+++ precache-manifest.2f28f4a541c8f60526686f9cc9d09941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "730668cf728b537b881b456e61ceec44",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "42e4aa818455a8a5855d",
+    "url": "/weather/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "eeb25b013d907b7fa7ea",
+    "url": "/weather/static/js/2.07f82822.chunk.js"
+  },
+  {
+    "revision": "42e4aa818455a8a5855d",
+    "url": "/weather/static/js/main.1c166952.chunk.js"
+  },
+  {
+    "revision": "34a118672da3072aec95",
+    "url": "/weather/static/js/runtime~main.1423296d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
